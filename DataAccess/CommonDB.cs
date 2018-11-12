eb71e13f8ea171039d9c692a4e15f8b99065ddd3
Linus Bruno Hagström Brosbøl <linu0043@edu.campusvejle.dk>
2018-11-12 10:33:36 +0100

    Created DataAccess library with CommonDB class

---
+++ DataAccess/CommonDB.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class CommonDB
+    {
+        private string connectionString;
+
+        public string ConnectionString
+        {
+            get { return connectionString; }
+            set { connectionString = value; }
+        }
+
+        public CommonDB(string constring)
+        {
+            ConnectionString = constring;
+        }
+        /// <summary>
+        /// Inserts data into the database using the SQL query
+        /// </summary>
+        /// <param name="queryString">Query string</param>
+        /// <returns>Returns the amount of rows affected in the process</returns>
+        protected int InsertDataToDB(string queryString)
+        {
+            int rowsAffected = 0;
+
+            using (SqlConnection c = new SqlConnection(connectionString))
+            {
+                c.Open();
+                using (SqlCommand com = new SqlCommand(queryString, c))
+                {
+                    rowsAffected = com.ExecuteNonQuery();
+                }
+            }
+            return rowsAffected;
+        }
+        /// <summary>
+        /// Gets data from the database using the SQL query
+        /// </summary>
+        /// <param name="queryString">Query string</param>
+        /// <returns>Returns a dataset of the data selected from the database</returns>
+        protected DataSet SelectDataFromDB(string queryString)
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection c = new SqlConnection(connectionString))
+            {
+                c.Open();
+                using (SqlDataAdapter adapter = new SqlDataAdapter(queryString, c))
+                {
+                    adapter.Fill(ds);
+                }
+            }
+            return ds;
+        }
+    }
+}
---
+++ DataAccess/DBHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DBHandler : CommonDB
+    {
+        /// <summary>
+        /// Creates a connection to the database
+        /// </summary>
+        /// <param name="constring">Connection string to connect to</param>
+        public DBHandler(string constring) : base(constring)
+        {
+        }
+
+        
+    }
+}
