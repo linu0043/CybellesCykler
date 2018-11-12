2157fa9a169b5dc15da5c1708b2fe0bae97f3d20
Linus Bruno Hagström Brosbøl <linu0043@edu.campusvejle.dk>
2018-11-12 13:41:31 +0100

    Created small interface

--- Business/DataController.cs
+++ Business/DataController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess;
 
 namespace Business
 {
     public class DataController
     {
+        private DBHandler handler;
 
+        public DBHandler Handler
+        {
+            get { return handler; }
+            set { handler = value; }
+        }
+
+        public DataController(string conString)
+        {
+            Handler.ConnectionString = conString;
+        }
     }
 }
